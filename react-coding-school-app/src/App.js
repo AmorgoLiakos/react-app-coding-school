82c025fa1a8649944865856070e0d9e5188d26c0
Liakos <liakos.prekas@gmail.com>
2021-02-21 13:04:52 +0200

    Cart items with map - one component at the moment

--- react-coding-school-app/src/App.js
+++ react-coding-school-app/src/App.js
@@ -1,16 +1,32 @@
+import { useState } from "react"
 import "./App.css"
-import Description from "./Components/Description"
-import FeaturedImage from "./Components/FeaturedImage"
-import QuantitySection from "./Components/QuantitySection"
+import CartItem from "./Components/CartItem"
 
 function App() {
-  return (
-    <>
-      <FeaturedImage />
-      <Description />
-      <QuantitySection />
-    </>
-  )
+  const [CartItems, setCartItems] = useState([
+    {
+      title: "cart item one",
+      price: "50",
+      description: "Desc for cart item one"
+    },
+    {
+      title: "cart item two",
+      price: "65",
+      description: "Desc for cart item two"
+    },
+    {
+      title: "cart item three",
+      price: "45",
+      description: "Desc for cart item three"
+    },
+    {
+      title: "cart item four",
+      price: "30",
+      description: "Desc for cart item four"
+    }
+  ])
+
+  return CartItems.map((x, index) => <CartItem title={CartItems[index].title} description={CartItems[index].description} price={CartItems[index].price} />)
 }
 
 export default App
---
+++ react-coding-school-app/src/Components/CartItem/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react"
+import "./CartItem.css"
+
+const CartItem = props => {
+  const [quantity, setQuantity] = useState(0)
+
+  const handleMinus = () => {
+    setQuantity(quantity - 1)
+  }
+
+  const handlePlus = () => {
+    setQuantity(quantity + 1)
+  }
+
+  return (
+    <div className="cart-item">
+      <div className="cart-item-img">
+        <img src="https://picsum.photos/300/300" alt="featIamge" />
+      </div>
+
+      <div className="description">
+        <h1>{props.title}</h1>
+        <p>{props.description}</p>
+        <h2>{props.price} €</h2>
+      </div>
+
+      <div className="quantity">
+        <button onClick={handleMinus}>-</button>
+        <div id="quantity">{quantity}</div>
+        <button onClick={handlePlus}>+</button>
+      </div>
+
+      <div className="total-price">
+        <h3>{quantity * props.price}</h3>
+      </div>
+    </div>
+  )
+}
+
+export default CartItem
