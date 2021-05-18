33aad39383392ab8f900d89e37460862f2608ae6
Liakos <liakos.prekas@gmail.com>
2021-05-18 22:23:57 +0300

    Some code Review Actions

--- react-coding-school-app/src/App.js
+++ react-coding-school-app/src/App.js
@@ -11,34 +11,35 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 import "./App.css"
 
-function App() {
-  const initialState = [
-    {
-      id: 1,
-      name: "Labrador Retriever!!",
-      price: "500",
-      description: "The Labrador Retriever, often abbreviated to Labrador, is a breed of retriever-gun dog from the United Kingdom that was developed from imported Canadian fishing dogs.",
-      imageURL: "img/labrador.jpg",
-      quantity: 0
-    },
-    {
-      id: 2,
-      name: "Husky",
-      price: "600",
-      description: "A husky is a sled dog used in the polar regions. One can differentiate huskies from other dog types by their fast pulling-style. They represent an ever-changing crossbreed of the fastest dogs[.",
-      imageURL: "img/husky.jpg",
-      quantity: 0
-    },
-    {
-      id: 3,
-      name: "German Shepherd",
-      price: "700",
-      description: "The German Shepherd (German: Deutscher Schäferhund, German pronunciation: [ˈdɔʏtʃɐ ˈʃɛːfɐˌhʊnt]) is a breed of medium to large-sized working dog that originated in Germany.",
-      imageURL: "img/sheperd.jpg",
-      quantity: 0
-    }
-  ]
+// This could be move outside of the function as normally this should come from an API endpoint
+const initialState = [
+  {
+    id: 1,
+    name: "Labrador Retriever!!",
+    price: "500",
+    description: "The Labrador Retriever, often abbreviated to Labrador, is a breed of retriever-gun dog from the United Kingdom that was developed from imported Canadian fishing dogs.",
+    imageURL: "img/labrador.jpg",
+    quantity: 0
+  },
+  {
+    id: 2,
+    name: "Husky",
+    price: "600",
+    description: "A husky is a sled dog used in the polar regions. One can differentiate huskies from other dog types by their fast pulling-style. They represent an ever-changing crossbreed of the fastest dogs[.",
+    imageURL: "img/husky.jpg",
+    quantity: 0
+  },
+  {
+    id: 3,
+    name: "German Shepherd",
+    price: "700",
+    description: "The German Shepherd (German: Deutscher Schäferhund, German pronunciation: [ˈdɔʏtʃɐ ˈʃɛːfɐˌhʊnt]) is a breed of medium to large-sized working dog that originated in Germany.",
+    imageURL: "img/sheperd.jpg",
+    quantity: 0
+  }
+]
 
+function App() {
   const [dogs, setDogs] = useState(initialState)
 
   const handleMinus = index => {
@@ -65,6 +66,7 @@ function App() {
 
   const [CartItems, setCartItems] = useState([])
 
+  // A hashmap can help you to easily make search & modifications
   const AddToCart = x => {
     setCartItems(prevCartItems => {
       let newCart = [...prevCartItems]
@@ -80,6 +82,7 @@ function App() {
         return newCart
       } else {
         newCart.push(newItem)
+        newCart.push({ ...newItem })
         return newCart
       }
     })
@@ -95,6 +98,8 @@ function App() {
     setTotalPrice(sum)
   }, [CartItems])
 
+  // handleMinus & handlePlus and quantity could be hidden inside the Dog component
+  // So the component handle the quantity state itself
   return (
     <TotalPriceContext.Provider value={[totalPrice, setTotalPrice]}>
       <CartItemsContext.Provider value={[CartItems, setCartItems]}>
